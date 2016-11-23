c13786a89e49c7dffe0de644c17873b5d1f70ab3
Dima Tichonov <dima@padv.local>
2016-11-23 17:32:25 +0200

    Add Ajax for insert new part to list

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>  
       <head>  
            <title>Live Table Data Edit</title>  
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-           
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+		           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+   
       </head>  
       <body>  
            <div class="container">  
@@ -21,9 +21,9 @@
  <script>  
  $(document).ready(function(){  
       function fetch_data()  
-      { 
+      {  
            $.ajax({  
-                url:"2.php",  
+                url:"select.php",  
                 method:"POST",  
                 success:function(data){  
                      $('#live_data').html(data);  
@@ -31,5 +31,37 @@
            });  
       }  
       fetch_data();  
+      $(document).on('click', '#btn_add', function(){  
+           var price = $('#price').text();  
+           var compatible_cars = $('#compatible_cars').text();  
+		   var information = $('#information').text();  
+           if(price == '')  
+           {  
+                alert("Enter First Name");  
+                return false;  
+           }  
+           if(compatible_cars == '')  
+           {  
+                alert("Enter Last Name");  
+                return false;  
+           }  
+           if(information == '')  
+           {  
+                alert("Enter Last Name");  
+                return false;  
+           } 
+           $.ajax({  
+                url:"insert.php",  
+                method:"POST",  
+                data:{price:price, compatible_cars:compatible_cars,information:information},  
+                dataType:"text",  
+                success:function(data)  
+                {  
+                     alert(data);  
+                     fetch_data();  
+                }  
+           })  
+      });  
+ 
  });  
  </script>  
