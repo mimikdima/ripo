5110710f75c500e2eceb7b8ad673bf9392004ff1
Dima Tichonov <dima@padv.local>
2016-11-23 17:02:04 +0200

    create tablee

--- create_table.php
+++ create_table.php
@@ -1,16 +1,7 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "test_matrix";
+include('conn.php');
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $db);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
 
 //Parts Table
 $sql = "CREATE TABLE parts (
