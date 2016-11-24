41075e6eb8f1aeefaf53ec4dfccdb12cee9c9643
Dima Tichonov <dima@padv.local>
2016-11-24 15:05:07 +0200

    add new parts

--- insert.php
+++ insert.php
@@ -1,6 +1,7 @@
 <?php  
 include('conn.php');
- $sql = "INSERT INTO parts(price, compatible_cars,information) VALUES('".$_POST["price"]."', '".$_POST["compatible_cars"]."', '".$_POST["information"]."')";  
+ $sql = "INSERT INTO parts(price, compatible_cars,on_stock,quantity,information) 
+		VALUES('".$_POST["price"]."', '".$_POST["compatible_cars"]."', '".$on_stock."', '".$_POST["quantity"]."', '".$_POST["information"]."')";  
  if(mysqli_query($conn, $sql))  
  {  
       echo 'New Part Inserted';  
