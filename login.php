1f22ea0bf5b54418fc743b7a4d2191b73058fa8f
Dima Tichonov <dima@padv.local>
2016-11-29 11:19:29 +0200

    login

--- login.php
+++ login.php
@@ -1,18 +1,9 @@
 <?php
- session_start();
+
 include('conn.php');
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-
-
-?>
-
-
-<?php
-$msg = '';
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 if (isset($_POST['login']) && !empty($_POST['username'])&& !empty($_POST['password'])) {
 	
@@ -23,12 +14,8 @@ if (isset($_POST['login']) && !empty($_POST['username'])&& !empty($_POST['passwo
 
 	if($result->num_rows > 0 )
 	{ 
-	
-	 
-//header( "refresh:1;url=index.php" );
-	//	$_SESSION["naam"] = $login; .
+	session_start();
 	 $_SESSION["manager"] = $login; 
-		echo 'ok';
 	}
 	else
 	{
@@ -39,10 +26,10 @@ if (isset($_POST['login']) && !empty($_POST['username'])&& !empty($_POST['passwo
 }
 
          ?>
-		 <html lang = "en">
+<html lang = "en">
    
    <head>
-      <title>TLogin</title>
+      <title>Login</title>
 
       
 
@@ -62,16 +49,13 @@ if (isset($_POST['login']) && !empty($_POST['username'])&& !empty($_POST['passwo
             action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
             ?>" method = "post">
             <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
-            <input type = "text" class = "form-control" 
-               name = "username" placeholder = "login" 
-               required autofocus></br>
-            <input type = "password" class = "form-control"
-               name = "password" placeholder = "password" required>
-            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
-               name = "login">Login</button>
+            <input type = "text" class = "form-control" name = "username" placeholder = "login" required autofocus></br>
+            <input type = "password" class = "form-control"name = "password" placeholder = "password" required></br></br>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "login">Login</button>
          </form>
+		<a href = "index.php" tite = "Main Page">Main Page</a></br></br></br>
 		<?}else{?>
-			<a href = "logout.php" tite = "Logout">Logout</a>
+			<a href = "index.php" tite = "Main Page">Main Page</a></br></br></br>
 		<?}?>
       </div> 
       
